c47108f3f14b9a78cd253bd195334a1d4da77d19
stevekutz <stkutz@gmail.com>
2021-02-14 19:59:41 -0600

    setting up AppLoading for loading fonts via Promise

--- reactnative_guess_num/App.js
+++ reactnative_guess_num/App.js
@@ -1,16 +1,49 @@
 // import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo'; // prolongss default loading of App screen until a particular task is done
+
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
+
+
+
+const fetchFonts = () => {
+    
+    // returns a Promise
+    Font.loadAsync({
+    'atures100' : require('./assets/fonts/Atures-100_PERSONAL_USE.ttf'),
+    'atures900' : require('./assets/fonts/Atures-900_PERSONAL_USE.ttf'),  // bolder
+    'hemihead' : require('./assets/fonts/HEMIHEAD.TTF')
+
+    });
+
+}
+
 
 export default function App() {
 
     // while userNumber exists, GameScreen component should be active , NOT ''
     const [userNumber, setUserNumber] = useState();
     const [guessAttempts, setGuessAttempts] =  useState(0);
+    const [dataLoaded, setDataLoaded] = useState(false);
+
+    if (!dataLoaded) {
+        return (
+            <AppLoading 
+                startAsync = {fetchFonts} 
+                onFinish = {() => setDataLoaded(true)}
+                onError = {() => console.log(err)}
+            />
+        
+        )
+        
+    }
+
 
     // currentGuess from GameScreen component should be passed into handler
     const startGameHandler = (selectedNumber) => {
